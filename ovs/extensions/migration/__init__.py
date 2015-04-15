d7c8fa1c36365bbdd4e8fdb06431f52fc3a1ce38
kvanhijf <kevin.vanhijfte@openvstorage.com>
2015-04-15 17:56:11 +0200

    OVS-1879 - Implemented MDSServiceController.remove_mds_service Added additional removal of certain config files/directories which weren't removed yet Template config files are now removed after Service has been added

---
+++ ovs/extensions/migration/__init__.py
@@ -0,0 +1,6 @@
+# Copyright 2014 CloudFounders NV
+# All rights reserved
+
+"""
+This package provides functionality for migrations
+"""
---
+++ ovs/extensions/plugins/asdmanager.py
@@ -0,0 +1,117 @@
+# Copyright 2015 CloudFounders NV
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Generic ALBA CLI module
+"""
+import json
+import base64
+import requests
+
+
+class ASDManagerClient(object):
+
+    def __init__(self, node):
+        self.node = node
+
+    def get_metadata(self):
+        """
+        Gets metadata from the node
+        """
+        self._refresh()
+        return requests.get('{0}/'.format(self._base_url),
+                            headers=self._base_headers,
+                            verify=False).json()
+
+    def get_ips(self):
+        """
+        Gets the ips from a node
+        """
+        self._refresh()
+        return requests.get('{0}/net'.format(self._base_url),
+                            verify=False).json()['ips']
+
+    def set_ips(self, ips):
+        """
+        Set primary storage ips
+        """
+        self._refresh()
+        requests.post('{0}/net'.format(self._base_url),
+                      data={'ips': json.dumps(ips)},
+                      headers=self._base_headers,
+                      verify=False)
+
+    def get_disks(self, as_list=True):
+        """
+        Gets the node's disk states
+        """
+        self._refresh()
+        disks = [] if as_list is True else {}
+        data = requests.get('{0}/disks'.format(self._base_url),
+                            headers=self._base_headers,
+                            verify=False).json()
+        for disk in data.keys():
+            if not disk.startswith('_'):
+                for key in data[disk].keys():
+                    if key.startswith('_'):
+                        del data[disk][key]
+                if as_list is True:
+                    disks.append(data[disk])
+                else:
+                    disks[disk] = data[disk]
+        return disks
+
+    def get_disk(self, disk):
+        """
+        Gets one of the node's disk's state
+        """
+        self._refresh()
+        data = requests.get('{0}/disks/{1}'.format(self._base_url, disk),
+                            headers=self._base_headers,
+                            verify=False).json()
+        for key in data.keys():
+            if key.startswith('_'):
+                del data[key]
+        return data
+
+    def add_disk(self, disk):
+        """
+        Adds a disk
+        """
+        self._refresh()
+        return requests.post('{0}/disks/{1}/add'.format(self._base_url, disk),
+                             headers=self._base_headers,
+                             verify=False).json()
+
+    def remove_disk(self, disk):
+        """
+        Removes a disk
+        """
+        self._refresh()
+        return requests.post('{0}/disks/{1}/delete'.format(self._base_url, disk),
+                             headers=self._base_headers,
+                             verify=False).json()
+
+    def restart_disk(self, disk):
+        """
+        Restarts a disk
+        """
+        self._refresh()
+        return requests.post('{0}/disks/{1}/restart'.format(self._base_url, disk),
+                             headers=self._base_headers,
+                             verify=False).json()
+
+    def _refresh(self):
+        self._base_url = 'https://{0}:{1}'.format(self.node.ip, self.node.port)
+        self._base_headers = {'Authorization': 'Basic {0}'.format(base64.b64encode('{0}:{1}'.format(self.node.username, self.node.password)).strip())}
