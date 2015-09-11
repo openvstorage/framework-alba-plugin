e0daddffcee974cf7fe8ede838860e07ca3e6d7c
Kenneth Henderick <kenneth@ketronic.be>
2015-09-11 09:53:51 +0200

    OVS-2847 - Decrease default NSM capacity to 50

--- ovs/dal/hybrids/j_nsmservice.py
+++ ovs/dal/hybrids/j_nsmservice.py
@@ -29,7 +29,7 @@ class NSMService(DataObject):
     * my_service.nsm_service.alba_backend
     """
     __properties = [Property('number', int, doc='The number of the service in case there are more than one'),
-                    Property('capacity', int, default=-1, doc='The capacity of this MDS, negative means infinite')]
+                    Property('capacity', int, default=50, doc='The capacity of this MDS, negative means infinite')]
     __relations = [Relation('alba_backend', AlbaBackend, 'nsm_services'),
                    Relation('service', Service, 'nsm_service', onetoone=True)]
     __dynamics = []
