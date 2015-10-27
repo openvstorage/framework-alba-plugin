664f490c1530b42b2208ea14938b1e68c3b1e5bd
eduardBM <cubusbacau@yahoo.com>
2015-10-27 12:22:46 +0200

    OVS-3191 Show asds when node is down - check that node is down - show asds as "Node unreachable" - allow to click Remove

--- ovs/dal/hybrids/albanode.py
+++ ovs/dal/hybrids/albanode.py
@@ -15,6 +15,7 @@
 """
 AlbaNode module
 """
+import requests
 from ovs.dal.dataobject import DataObject
 from ovs.dal.structures import Property, Relation, Dynamic
 from ovs.dal.hybrids.storagerouter import StorageRouter
@@ -55,28 +56,32 @@ class AlbaNode(DataObject):
         """
         Returns a live list of all disks on this node
         """
-
-        disks = self.client.get_disks()
-        # TODO: report node down
-        # if node down !!!
-        if disks == []:
+        try:
+            disks = self.client.get_disks(reraise=True)
+        except requests.ConnectionError:
             from ovs.dal.lists.albabackendlist import AlbaBackendList
+            disks = []
             for backend in AlbaBackendList.get_albabackends():
                 # All backends of this node
                 config_file = '/opt/OpenvStorage/config/arakoon/{0}-abm/{0}-abm.cfg'.format(backend.name)
                 osds = AlbaCLI.run('list-osds', config=config_file, as_json=True)
                 for osd in osds:
                     if osd.get('node_id') == self.node_id:
-                        disks.append({'asd_id': osd.get('long_id'),
+                        asd_id = osd.get('long_id')
+                        if osd.get('decommissioned') is True:
+                            state = {'state': 'decommissioned'}
+                        else:
+                            state = {'state': 'error', 'detail': 'nodedown'}
+                        disks.append({'asd_id': asd_id,
                                       'node_id': osd.get('node_id'),
                                       'port': osd.get('port'),
                                       'available': False,
-                                      'state': {'state': 'error', 'detail': 'node down'},
+                                      'state': state,
                                       'log_level': 'info',
-                                      'device': osd.get('long_id'),
-                                      'home': osd.get('long_id'),
-                                      'mountpoint': osd.get('long_id'),
-                                      'name': osd.get('long_id'),
+                                      'device': asd_id,
+                                      'home': asd_id,
+                                      'mountpoint': asd_id,
+                                      'name': asd_id,
                                       'usage': {'available': 0, 'size': 0, 'used': 0},
                                       })
         return disks
