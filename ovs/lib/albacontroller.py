d6f8628c08e2afd03e47a79adfe44ceef366c7f0
Kenneth Henderick <kenneth@ketronic.be>
2015-12-09 09:55:51 +0100

    OVS-3752 - Run the NSM checkup every hour

--- ovs/lib/albacontroller.py
+++ ovs/lib/albacontroller.py
@@ -532,8 +532,8 @@ class AlbaController(object):
             AlbaController._remove_service('rebalancer', client.ip, service_name, alba_backend.backend.name)
 
     @staticmethod
-    @celery.task(name='alba.nsm_checkup', schedule=crontab(minute='30', hour='0'))
-    @ensure_single(task_name='alba.nsm_checkup')
+    @celery.task(name='alba.nsm_checkup', schedule=crontab(minute='45', hour='*'))
+    @ensure_single(task_name='alba.nsm_checkup', mode='CHAINED')
     def nsm_checkup():
         """
         Validates the current NSM setup/configuration and takes actions where required.
